1ac5aaec8bb2257af096fe34a957d64578fb18d8
Doug Mamilor <dougmamilor@icloud.com>
2025-10-20 20:25:39 -0400

    feat: add type aliad for points

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,11 +1,13 @@
 pub mod ivec2;
 pub mod mat2;
 pub mod mat3;
+pub mod point;
 pub mod vec2;
 pub mod vec3;
 
 pub use ivec2::IVec2;
 pub use mat2::Mat2;
 pub use mat3::Mat3;
+pub use point::{Point2, Point3};
 pub use vec2::Vec2;
 pub use vec3::Vec3;
---
+++ src/math/point.rs
@@ -0,0 +1,4 @@
+use crate::math::{Vec2, Vec3};
+
+pub type Point2 = Vec2;
+pub type Point3 = Vec3;
